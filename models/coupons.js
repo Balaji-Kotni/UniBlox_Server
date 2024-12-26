57dd5c7ad94f5691a9cb4cebb660e8ce22e092be
Balaji Kotni <kotnivenkatabalaji@gmail.com>
2024-12-26 22:43:54 +0530

    Added Coupons Model

---
+++ models/coupons.js
@@ -0,0 +1,35 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../db";
+
+const Coupons = sequelize.define(
+  "coupons",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    code: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    discount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM("Active", "Inactive"),
+      allowNull: false,
+      defaultValue: "Active",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default Coupons;
